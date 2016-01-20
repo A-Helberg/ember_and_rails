e6a4083da91e4180c2ab34a3c301a474233d6fa8
Andre <helberg.andre@gmail.com>
2016-01-20 19:41:19 +0200

    use new ember class in model initializer

--- config/initializers/models.rb
+++ config/initializers/models.rb
@@ -1,21 +1,9 @@
 
-a = ActiveSupport::FileUpdateChecker.new([], {"#{Rails.root.join('app', 'models')}" => ["rb"]}) do
-  puts ""
-  puts "========================================================="
-  puts "Generating models"
-  Rails.application.eager_load!
-  ActiveRecord::Base.descendants.each do |model|
-    puts model.name
-    model.columns_hash.each {|k,v| puts "#{k} => #{v.type}"}
-  end
-  puts "========================================================="
-  puts ""
-end
 
   if Rails.configuration.cache_classes
     puts "Generating models"
   else
     Rails.application.config.to_prepare do
-      a.execute
+      Ember.update_files
     end
   end
