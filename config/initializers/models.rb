5a4f12d6d64397189885d1fb59874e9c2da5765c
Andre <helberg.andre@gmail.com>
2016-01-20 18:51:21 +0200

    add file watcher that outputs all models and their attributes

--- config/initializers/models.rb
+++ config/initializers/models.rb
@@ -1,8 +1,21 @@
 
+a = ActiveSupport::FileUpdateChecker.new([], {"#{Rails.root.join('app', 'models')}" => ["rb"]}) do
+  puts ""
+  puts "========================================================="
+  puts "Generating models"
+  Rails.application.eager_load!
+  ActiveRecord::Base.descendants.each do |model|
+    puts model.name
+    model.columns_hash.each {|k,v| puts "#{k} => #{v.type}"}
+  end
+  puts "========================================================="
+  puts ""
+end
+
   if Rails.configuration.cache_classes
     puts "Generating models"
   else
-    ActionDispatch::Reloader.to_prepare do
-      puts "Generating models"
+    Rails.application.config.to_prepare do
+      a.execute
     end
   end
